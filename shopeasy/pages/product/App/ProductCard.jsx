501d95e33ff3bc0e5bd7740a4053bfc282f34cab
Govind Singh <govindkmr141@gmail.com>
2022-11-13 23:14:47 +0530

    testing

--- shopeasy/pages/product/App/ProductCard.jsx
+++ shopeasy/pages/product/App/ProductCard.jsx
@@ -18,7 +18,7 @@ import PriceTag from "./PriceTag";
 
 export default function ProductCard(props) {
   const { product, rootProps } = props;
-  const { name, image, price, ratings, discount_price } = product;
+  // const { name, image, price, ratings, discount_price } = product;
   return (
     <Stack
       spacing={useBreakpointValue({
@@ -30,8 +30,8 @@ export default function ProductCard(props) {
       <Box position="relative">
         <AspectRatio ratio={4 / 3}>
           <Image
-            src={image}
-            alt={name}
+            src={product?.image}
+            alt={product?.name}
             draggable="false"
             fallback={<Skeleton />}
             borderRadius={useBreakpointValue({
@@ -44,7 +44,7 @@ export default function ProductCard(props) {
           position="absolute"
           top="4"
           right="4"
-          aria-label={`Add ${name} to your favourites`}
+          aria-label={`Add ${product?.name} to your favourites`}
         />
       </Box>
       <Stack>
@@ -53,14 +53,18 @@ export default function ProductCard(props) {
             fontWeight="medium"
             color={useColorModeValue("gray.700", "gray.400")}
           >
-            {name.slice(0, 25)}
+            {product?.name.slice(0, 25)}
           </Text>
-          <PriceTag price={price} salePrice={discount_price} currency="INR" />
+          <PriceTag
+            price={product?.price}
+            salePrice={product?.discount_price}
+            currency="INR"
+          />
         </Stack>
         <HStack>
-          <Rating defaultValue={ratings} size="sm" />
+          <Rating defaultValue={product?.ratings} size="sm" />
           <Text fontSize="sm" color={useColorModeValue("gray.600", "gray.400")}>
-            {ratings} Reviews
+            {product?.ratings} Reviews
           </Text>
         </HStack>
       </Stack>
